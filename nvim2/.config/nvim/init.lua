53a9182a4a09d4969d7a38323d4eef969a4a91b1
FilipHarald <filip.harald@gmail.com>
2025-12-02 21:06:42 +0100

    lazyvim

--- nvim2/.config/nvim/init.lua
+++ nvim2/.config/nvim/init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Keymaps
+vim.keymap.set('n', '<leader>air', function()
+  vim.fn.setreg('+', '')
+  vim.notify("Clipboard cleared!")
+end, { noremap = true })
+
+vim.keymap.set('n', '<leader>ain', function()
+  vim.fn.setreg('+', vim.fn.getreg('+') .. '\n' .. vim.fn.expand('%:p'))
+  vim.notify("Filename appended to clipboard!")
+end, { noremap = true })
--- nvim2/.config/nvim/lua/plugins/fzf.lua
+++ nvim2/.config/nvim/lua/plugins/fzf.lua
@@ -1,18 +1,32 @@
 return {
-  "ibhagwan/fzf-lua",
-  opts = {
-    fzf_opts = {
-      ["--no-scrollbar"] = true,
-      ["--history"] = vim.fn.stdpath("data") .. "/fzf-history",
-    },
-    winopts = {
-      width = 0.9,
-      height = 0.9,
-      row = 0.5,
-      col = 0.5,
-      preview = {
-        scrollchars = { "┃", "" },
-      },
-    },
-  },
+	"ibhagwan/fzf-lua",
+	opts = {
+		fzf_opts = {
+			["--no-scrollbar"] = true,
+			["--history"] = vim.fn.stdpath("data") .. "/fzf-history",
+		},
+		winopts = {
+			width = 0.9,
+			height = 0.9,
+			row = 0.5,
+			col = 0.5,
+			preview = {
+				scrollchars = { "┃", "" },
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader><leader>f",
+			function()
+				local current_file = vim.fn.expand("%")
+				local current_dir = vim.fn.fnamemodify(current_file, ":h")
+				local relative_dir = vim.fn.fnamemodify(current_dir, ":.")
+				require("fzf-lua").files({
+					query = relative_dir .. "/",
+				})
+			end,
+			desc = "Find neighboring files",
+		},
+	},
 }
