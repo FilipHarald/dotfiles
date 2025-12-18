0c498629ee5f96133dba3cc2b56e1ee1a57af3db
FilipHarald <filip.harald@gmail.com>
2025-12-18 20:15:42 +0100

    added mason files

---
+++ nvim2/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- Language Servers (most are auto-installed by LazyVim lang extras)
+        "ansible-language-server",
+        "ansible-lint",
+        "copilot-language-server",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "eslint-lsp",
+        "jinja-lsp",
+        "json-lsp",
+        "lua-language-server",
+        "marksman",
+        "nomicfoundation-solidity-language-server",
+        "typescript-language-server",
+        "vtsls",
+        "yaml-language-server",
+
+        -- Formatters
+        "shfmt",
+        "stylua",
+
+        -- Linters
+        "hadolint",
+        "markdownlint-cli2",
+        "solhint",
+
+        -- Tools
+        "markdown-toc",
+      },
+    },
+  },
+}
