08cd6ceeb4d4fdad563d6dad070a14014c1ddc59
filip <filip.harald@gmail.com>
2025-12-29 12:56:12 +0100

    testing with jk as escape

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,3 @@
 vim.keymap.set("n", "<leader>z", "", { desc = "FILIP" })
+vim.keymap.set("t", "jk", "<C-\\><C-n>", { noremap = true, silent = true })
+vim.keymap.set({ 'i', 'v' }, 'jk', '<Esc>', { noremap = true, silent = true })
