b62e1903475c378bc953e6de3f76920c43db96b5
filip <filip.harald@gmail.com>
2026-05-10 13:31:09 +0200

    removed opencode stats from nvim startup

--- nvim/.config/nvim/lua/plugins/snacks-dashboard.lua
+++ nvim/.config/nvim/lua/plugins/snacks-dashboard.lua
@@ -1,154 +1,156 @@
 return {
-  "folke/snacks.nvim",
-  ---@type snacks.Config
-  opts = {
-    dashboard = {
-      sections = {
-        {
-          pane = 1,
-          { title = "CWD: " .. vim.fn.getcwd(), padding = 2 },
-          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
-          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 2 },
-          {
-            icon = "󰂫 ",
-            title = "Obsidian",
-            padding = 2,
-            {
-              desc = "Dailies",
-              indent = 2,
-              key = "d",
-              action = function()
-                require("lazy").load({ plugins = { "obsidian.nvim" } })
-                vim.cmd("Obsidian dailies -20 8")
-              end,
-            },
-            {
-              desc = "Yesterday",
-              indent = 2,
-              key = "y",
-              action = function()
-                require("lazy").load({ plugins = { "obsidian.nvim" } })
-                vim.cmd("Obsidian yesterday")
-              end,
-            },
-            {
-              desc = "Today",
-              indent = 2,
-              key = "t",
-              action = function()
-                require("lazy").load({ plugins = { "obsidian.nvim" } })
-                vim.cmd("Obsidian today")
-              end,
-            },
-            {
-              desc = "Tomorrow",
-              indent = 2,
-              key = "w",
-              action = function()
-                require("lazy").load({ plugins = { "obsidian.nvim" } })
-                vim.cmd("Obsidian tomorrow")
-              end,
-            },
-            {
-              desc = "Obsidian Meeting",
-              indent = 2,
-              key = "m",
-              action = function()
-                require("lazy").load({ plugins = { "obsidian.nvim" } })
-                vim.cmd("Obsidian new \"Meeting\"")
-              end,
-            },
-          },
-          {
-            title = "Keys",
-            icon = " ",
-            section = "keys",
-            padding = 2,
-            indent = 2,
-          },
-          { section = "startup" },
-        },
-        {
-          pane = 2,
-          icon = " ",
-          desc = "Browse Repo",
-          padding = 1,
-          key = "b",
-          action = function()
-            Snacks.gitbrowse()
-          end,
-        },
-        function()
-          local in_git = Snacks.git.get_root() ~= nil
-          local cmds = {
-            {
-              -- requires: `gh ext install meiji163/gh-notify`
-              title = "Notifications",
-              cmd = "gh notify -s -n 3",
-              action = function()
-                vim.ui.open("https://github.com/notifications")
-              end,
-              key = "N",
-              icon = " ",
-              height = 5,
-              enabled = true,
-            },
-            {
-              title = "Open Issues",
-              cmd = "gh issue list -L 3",
-              key = "i",
-              action = function()
-                vim.fn.jobstart("gh issue list --web", { detach = true })
-              end,
-              icon = " ",
-              height = 7,
-            },
-            {
-              icon = " ",
-              title = "Open PRs",
-              cmd = "gh pr list -L 3",
-              key = "P",
-              action = function()
-                vim.fn.jobstart("gh pr list --web", { detach = true })
-              end,
-              height = 7,
-            },
-            {
-              pane = 2,
-              icon = " ",
-              title = "Git Status",
-              section = "terminal",
-              enabled = function()
-                return Snacks.git.get_root() ~= nil
-              end,
-              cmd = "git status --short --branch --renames",
-              height = 5,
-              padding = 1,
-              ttl = 5 * 60,
-              indent = 3,
-            },
-          }
-          return vim.tbl_map(function(cmd)
-            return vim.tbl_extend("force", {
-              pane = 2,
-              section = "terminal",
-              enabled = in_git,
-              padding = 1,
-              ttl = 5 * 60,
-              indent = 3,
-            }, cmd)
-          end, cmds)
-        end,
-        {
-          pane = 3,
-          icon = " ",
-          title = "opencode stats",
-          cmd = "opencode stats",
-          section = "terminal",
-          ttl = 60 * 60,
-          height = 40,
-        }
-      },
-    }
-  }
+	"folke/snacks.nvim",
+	---@type snacks.Config
+	opts = {
+		dashboard = {
+			sections = {
+				{
+					pane = 1,
+					{ title = "CWD: " .. vim.fn.getcwd(), padding = 2 },
+					{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+					{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 2 },
+					{
+						icon = "󰂫 ",
+						title = "Obsidian",
+						padding = 2,
+						{
+							desc = "Dailies",
+							indent = 2,
+							key = "d",
+							action = function()
+								require("lazy").load({ plugins = { "obsidian.nvim" } })
+								vim.cmd("Obsidian dailies -20 8")
+							end,
+						},
+						{
+							desc = "Yesterday",
+							indent = 2,
+							key = "y",
+							action = function()
+								require("lazy").load({ plugins = { "obsidian.nvim" } })
+								vim.cmd("Obsidian yesterday")
+							end,
+						},
+						{
+							desc = "Today",
+							indent = 2,
+							key = "t",
+							action = function()
+								require("lazy").load({ plugins = { "obsidian.nvim" } })
+								vim.cmd("Obsidian today")
+							end,
+						},
+						{
+							desc = "Tomorrow",
+							indent = 2,
+							key = "w",
+							action = function()
+								require("lazy").load({ plugins = { "obsidian.nvim" } })
+								vim.cmd("Obsidian tomorrow")
+							end,
+						},
+						{
+							desc = "Obsidian Meeting",
+							indent = 2,
+							key = "m",
+							action = function()
+								require("lazy").load({ plugins = { "obsidian.nvim" } })
+								vim.cmd('Obsidian new "Meeting"')
+							end,
+						},
+						{
+							desc = "Search Meetings",
+							indent = 2,
+							key = "h",
+							action = function()
+								require("lazy").load({ plugins = { "fzf-lua" } })
+								require("fzf-lua").files({
+									cwd = vim.fn.expand("~/Documents/obsidian/filip/notes/meetings"),
+								})
+							end,
+						},
+					},
+					{
+						title = "Keys",
+						icon = " ",
+						section = "keys",
+						padding = 2,
+						indent = 2,
+					},
+					{ section = "startup" },
+				},
+				{
+					pane = 2,
+					icon = " ",
+					desc = "Browse Repo",
+					padding = 1,
+					key = "b",
+					action = function()
+						Snacks.gitbrowse()
+					end,
+				},
+				function()
+					local in_git = Snacks.git.get_root() ~= nil
+					local cmds = {
+						{
+							-- requires: `gh ext install meiji163/gh-notify`
+							title = "Notifications",
+							cmd = "gh notify -s -n 3",
+							action = function()
+								vim.ui.open("https://github.com/notifications")
+							end,
+							key = "N",
+							icon = " ",
+							height = 5,
+							enabled = true,
+						},
+						{
+							title = "Open Issues",
+							cmd = "gh issue list -L 3",
+							key = "i",
+							action = function()
+								vim.fn.jobstart("gh issue list --web", { detach = true })
+							end,
+							icon = " ",
+							height = 7,
+						},
+						{
+							icon = " ",
+							title = "Open PRs",
+							cmd = "gh pr list -L 3",
+							key = "P",
+							action = function()
+								vim.fn.jobstart("gh pr list --web", { detach = true })
+							end,
+							height = 7,
+						},
+						{
+							pane = 2,
+							icon = " ",
+							title = "Git Status",
+							section = "terminal",
+							enabled = function()
+								return Snacks.git.get_root() ~= nil
+							end,
+							cmd = "git status --short --branch --renames",
+							height = 5,
+							padding = 1,
+							ttl = 5 * 60,
+							indent = 3,
+						},
+					}
+					return vim.tbl_map(function(cmd)
+						return vim.tbl_extend("force", {
+							pane = 2,
+							section = "terminal",
+							enabled = in_git,
+							padding = 1,
+							ttl = 5 * 60,
+							indent = 3,
+						}, cmd)
+					end, cmds)
+				end,
+			},
+		},
+	},
 }
