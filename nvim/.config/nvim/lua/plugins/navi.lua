7ed1f55d099c86e0eb6c4ed81655a4da35bc6a53
filip <filip.harald@gmail.com>
2026-07-22 09:14:12 +0200

    rebind navi next/prev to <leader>nn/<leader>nN to avoid sidekick Tab collision

--- nvim/.config/nvim/lua/plugins/navi.lua
+++ nvim/.config/nvim/lua/plugins/navi.lua
@@ -2,8 +2,8 @@ return {
   "kitlangton/navi.nvim",
   cmd = { "NaviLoad", "NaviNext", "NaviPrev", "NaviPick", "NaviClear", "NaviTest" },
   keys = {
-    { "<Tab>", "<Cmd>NaviNext<CR>", desc = "Next Navi stop" },
-    { "<S-Tab>", "<Cmd>NaviPrev<CR>", desc = "Previous Navi stop" },
+    { "<leader>nn", "<Cmd>NaviNext<CR>", desc = "Next Navi stop" },
+    { "<leader>nN", "<Cmd>NaviPrev<CR>", desc = "Previous Navi stop" },
     { "<leader>np", "<Cmd>NaviPick<CR>", desc = "Pick Navi stop" },
     { "<leader>nc", "<Cmd>NaviClear<CR>", desc = "Clear Navi tour" },
   },
